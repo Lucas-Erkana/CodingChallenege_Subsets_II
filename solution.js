cf7655e0eb8ccff9810624dc6004e277cbed8868
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-06 09:08:11 +0200

    Solution on 6 November 2023

--- solution.js
+++ solution.js
@@ -1,9 +1,34 @@
-var functionname = function(parameter) {
-//Write code here
-  };
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+  nums.sort((a, b) => a - b); // Sort the input array
+
+const result = []; // Initialize result array
+
+// Recursive function to generate subsets
+function generateSubsets(startIndex, currentSubset, nums) {
+ result.push(currentSubset); // Add currentSubset to result array
+
+ for (let i = startIndex; i < nums.length; i++) {
+   // Skip duplicates
+   if (i > startIndex && nums[i] === nums[i - 1]) {
+     continue;
+   }
+
+   // Recursively generate subsets
+   generateSubsets(i + 1, currentSubset.concat(nums[i]), nums);
+ }
+}
+
+generateSubsets(0, [], nums); // Call the recursive function
+
+return result; // Return the result array
+};
 
 
-  let parameter = 'test data'
-console.log(functionname(parameter)) //expected result true
-
-module.exports = functionname;
+console.log(subsetsWithDup([1, 2, 2])); // Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+console.log(subsetsWithDup([0])); // Output: [[],[0]]
+console.log(subsetsWithDup([-1, 2, -1])); // Output: [ [], [ -1 ], [ -1, -1 ], [ -1, -1, 2 ], [ -1, 2 ], [ 2 ] ]
+module.exports = subsetsWithDup;
--- tests.js
+++ tests.js
@@ -1,6 +1,46 @@
-const funtionname = require('./solution');
+const subsetsWithDup = require('./solution');
 const assert = require('assert');
 
 describe('Tests', function () {
-//Write tests here
+  it('Test Case 1: Empty Array', function () {
+    const input = [];
+    const expectedOutput = [[]];
+    const actualOutput = subsetsWithDup(input);
+    assert.deepStrictEqual(actualOutput, expectedOutput);
+  });
+
+  it('Test Case 2: Single Element Array', function () {
+    const input = [0];
+    const expectedOutput = [[], [0]];
+    const actualOutput = subsetsWithDup(input);
+    assert.deepStrictEqual(actualOutput, expectedOutput);
+  });
+
+  it('Test Case 3: Array with Duplicates', function () {
+    const input = [1, 2, 2];
+    const expectedOutput = [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]];
+    const actualOutput = subsetsWithDup(input);
+    assert.deepStrictEqual(actualOutput, expectedOutput);
+  });
+
+  it('Test Case 4: Array with Negative Numbers', function () {
+    const input = [-1, 2, -1];
+    const expectedOutput = [ [], [ -1 ], [ -1, -1 ], [ -1, -1, 2 ], [ -1, 2 ], [ 2 ] ];
+    const actualOutput = subsetsWithDup(input);
+    assert.deepStrictEqual(actualOutput, expectedOutput);
+  });
+
+  it('Test Case 5: Array with All Elements Same', function () {
+    const input = [5, 5, 5];
+    const expectedOutput = [[], [5], [5, 5], [5, 5, 5]];
+    const actualOutput = subsetsWithDup(input);
+    assert.deepStrictEqual(actualOutput, expectedOutput);
+  });
+
+  it('Test Case 6: Array with Unique Elements', function () {
+    const input = [1, 2, 3];
+    const expectedOutput = [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]];
+    const actualOutput = subsetsWithDup(input);
+    assert.deepStrictEqual(actualOutput, expectedOutput);
+  });
 });
